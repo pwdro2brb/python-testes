e974b9902a9c2131548c53963270fddfd983acb7
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-07-26 20:50:58 -0300

    Números e algumas funções em python

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-name = 'Pedro'
+n = 50
+n2 = str(n)
 
-print(' é um garoto. \'bem\' \n Macho <-!!->')
-print(name[3])
-print(name.upper())
-print(name.lower())
-print(name.isupper())
-print(name.upper().isupper())
-print(len(name))
-print(name.index('d'))
-print(name.replace('pedro', 'he'))
-print(name.replace('Pedro', 'he'))
+print(45 + 37 , n)
+print(45 + 37 + n)
+print(45 + 37 - n)
+print(45 + 37 * n)
+print(45 + 37 / n)
+
+print(20%6)
+print('bin salabinbinbi ó número saido emfim ' + n2)
+
+print(abs(-684766746887))
+
+print(max(45,87,6987,34,9*9*3*4*9*99))
+
+print(min(9876,9+9,8/8,9-4687+398909*58-0-590*690*6580-650*0,-100000000000))
+
+print(round(3456,96))
+
+print(bin(12398479837454379853984653))
+
+from math import *
+print(sqrt(225))
+
+
+
+
