6a49a5ad651f790f0f6a806e333c3cc3782a614e
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-07-28 19:46:32 -0300

    Input em python

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-n = 50
-n2 = str(n)
+input('Digite aqui o país com a maior população: ')
 
-print(45 + 37 , n)
-print(45 + 37 + n)
-print(45 + 37 - n)
-print(45 + 37 * n)
-print(45 + 37 / n)
+n = input('Digite seu cpf: ')
+B = input('Digite seu nome: ')
+c = int(input('Digite sua idade: '))
 
-print(20%6)
-print('bin salabinbinbi ó número saido emfim ' + n2)
-
-print(abs(-684766746887))
-
-print(max(45,87,6987,34,9*9*3*4*9*99))
-
-print(min(9876,9+9,8/8,9-4687+398909*58-0-590*690*6580-650*0,-100000000000))
-
-print(round(3456,96))
-
-print(bin(12398479837454379853984653))
-
-from math import *
-print(sqrt(225))
-
-
+print('O seu nome é '+ B +' e sua idade é ', c ,'e com o cpf como '+ n)
 
 
