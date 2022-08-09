6d94321ab6109a0982b611b9460ab52310f30132
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-09 17:49:37 -0300

    Funções para listas em python

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-y = ['1', ' guei', ' alóóóóó', ' Brasil',' CAIXA CAIXA', 'Delta', True, 78]
+y = ['1', ' guei', ' alóóóóó', 2,2,2,2,2,2,2,2,2,2,
+    2, 2, ' Brasil',' CAIXA CAIXA', 'Delta', True, 78]
 
+z=[1,2,3,6,"golira"]
+
+z.extend(y)
+
+print(z)
+
+print(' ')
+
+y.append(False)
+print(y)
+print(z)
+
+print(' ')
+
+
+y.insert(3, 'A divina comédia')
 print(y)
 
 print(' ')
-y[2] = input(' digite qualquer coisa aí ')
+
+
+print(y.count(2))
+
 print(' ')
 
-print(y[0:5])
-print(y[-3])
-print(type(y))
-print(type(y[6]))
-print(type(y[7]))
-print(len(y))
+
+z.clear()
+print(z)
+
+print(' ')
+
+zz = [5,87,34,2,3,45,2,1,3,4,12,43,4,2,4,2,1,2,2,3,4,5,6,7,88,9,]
+zz.sort()
+print(zz)
+
+print(' ')
+
+a = z.copy()
+print(a)
+
+
+print(' ')
+
+y.pop(0)
+print(y)
+
+print(' ') 
+
+
+
+print(' ')
