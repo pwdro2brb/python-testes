74b01d77c36aa4e2ef39e50fd5074a622532fba2
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-09 18:10:09 -0300

    tuples(enúpulos) em python

--- app.py
+++ app.py
@@ -1,54 +1,29 @@
-y = ['1', ' guei', ' alóóóóó', 2,2,2,2,2,2,2,2,2,2,
-    2, 2, ' Brasil',' CAIXA CAIXA', 'Delta', True, 78]
-
-z=[1,2,3,6,"golira"]
-
-z.extend(y)
-
-print(z)
+b = (7,'d', True, "me ajuda aí", 8)
+c = (8,'e', False, "He need some milk", 79)
+d = (9,'f', True, "Brasil \o/", 34)
 
 print(' ')
 
-y.append(False)
-print(y)
-print(z)
+print(b)
 
 print(' ')
 
-
-y.insert(3, 'A divina comédia')
-print(y)
+print(b[3])
 
 print(' ')
 
-
-print(y.count(2))
-
-print(' ')
-
-
-z.clear()
-print(z)
+print(len(c))
 
 print(' ')
 
-zz = [5,87,34,2,3,45,2,1,3,4,12,43,4,2,4,2,1,2,2,3,4,5,6,7,88,9,]
-zz.sort()
-print(zz)
+print(type(d))
 
 print(' ')
 
-a = z.copy()
-print(a)
-
+print(type(b[4]))
 
 print(' ')
 
-y.pop(0)
-print(y)
-
-print(' ') 
-
-
+print(d)
 
 print(' ')
