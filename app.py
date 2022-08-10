dbf648792971aa901893f5bc5cb2bdefa1d0580a
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-10 20:59:25 -0300

    Condições if em python

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-def oi_function(nome,idade,pergunta):
-   return nome+idade/pergunta
 
-nome = int(input('Digite um number '))
-idade = int(input('Digite um outro número'))
-pergunta = int(input('Número '))
+d = int(input(' d = '))
+c = int(input(' c = '))
+b = input(' b = ')
+a = input(' a = ')
+
 
 print(' ')
 
-print(oi_function(nome,idade,pergunta))
+if a < b:
+   print(str(a) +' é maior que '+ str(b))
+elif a == b:
+   print(str(a) + ' = '+str(b))
+else:
+   print('a não é maior que b')
+
+print(' ')
+if a := b:
+   print(' a é igual a b ')
+
+print(' ')
+
+if type(c) == str:
+   print('O valor de c digitado é um string')
+else:
+   print('O valor de c digitado não é um string')
+
+print(' ')
+
+if d%5==0:
+   print('O valor de d é divisivel por 5')
+else:
+   print('O valor de d não é divisivel por 5')
+
 
 print(' ')
