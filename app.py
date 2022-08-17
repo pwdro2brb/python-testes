4460144bb2f48d1fa90047fe30888f7e6c8e9e0c
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-17 19:16:55 -0300

    leitura de otros arquivos dentro do python

--- app.py
+++ app.py
@@ -1,44 +1,20 @@
-from ast import Try
-import math
-
-print(' ')
-try:
- a = int(input('Digite um número: '))
- calculo = input('Digite o calculo desejado, ou sinal: ')
- b = int(input('Digite o segundo número: '))
-
-except:
-   print(' ')
-   print('Não é um número ')
- 
-
 print(' ')
 
+arqu_pa = open("C:/Users/User/Desktop/python testes/paises.txt", "r", encoding="utf8")#abre o outro arquivo nesse outro local
+print(arqu_pa.readline()[0])#inprime a primeira linha e a primeira letra do outro documento
 
-try:
-   
- if calculo == '+':
-   print('O resutado da soma será de ',a+b)
+print(' ')
+print(arqu_pa.readline())#Agora ele irá inprimir a segunda linha do outro documento
+print(arqu_pa.readline(3))#terceira linha e os trés primeiros valores dela 
+print(' ')
 
- elif calculo == '-':
-    print('O resultado da subtração será de ',a-b)    
+print(arqu_pa.readable())#inprime se o outro arquivo é "legivel"
+print(' ')
 
- elif calculo == '/' and b !=0 :
-    print('O resultado da divisão será de ',a/b)
-
- elif calculo == '*':
-    print('O resultado da multiplicação será de ',a*b)
-
- elif calculo == 'potencia' or calculo == 'Potência' or calculo == 'potência' or calculo == 'POTENCIA'or calculo == 'POTÊNCIA'or calculo == ' Potência'or calculo == ' potencia'or calculo == ' potência'or calculo == '^'or calculo == 'elevado por'or calculo == 'Elevado por':
-    print('O primeiro número elevado pelo segundo terá o resultado de: ',math.pow(a, b))
-
- elif calculo == 'raiz' or calculo == 'Raiz' or calculo == 'RAIZ' or calculo == 'raiz quadrada'or calculo == 'Raiz quadrada'or calculo == 'raiz Quadrada'or calculo == 'Raiz Quadrada'or calculo == 'RAIZ quadrada'or calculo == 'raiz QUADRADA'or calculo == 'RAIZ QUADRADA'or calculo == 'Raiz QUADRADA':
-    print('A raiz quadrada do primeiro número é = ',math.sqrt(a),'. e a do segundo número será = ', math.sqrt(b) )
-
- else:
-    print('Não possuirá valor numerico')
-except:
-   print(' ')
+for files in arqu_pa.readlines():#inprime todas as linhas restantes com espaço (quebra de linha)
+   print(files)
+arqu_pa.close
+print(' ')
 
 
 
