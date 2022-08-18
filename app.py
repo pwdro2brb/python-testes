76419b82599424d3e643092525bb8528b6adfbf8
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-18 18:48:31 -0300

    classes e objetos em python

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 print(' ')
 
-arqu_pa = open("C:/Users/User/Desktop/python testes/paises.txt", "r", encoding="utf8")#abre o outro arquivo nesse outro local, fará a sua leitura, e considera a codificação do outro arquivo.
+class Person:
+  def __init__(si, nome, idade,tele):
+    si.nome = nome
+    si.idade = idade
+    si.tele = tele
 
-print(arqu_pa.readline()[0])#inprime a primeira linha e a primeira letra do outro documento
+p = Person("John", 36, 14975432462)
+
+print(p.nome)
+print(p.idade)
+print(p.tele)
+
 
 print(' ')
 
-print(arqu_pa.readline())#Agora ele irá inprimir a segunda linha do outro documento
-arqu_pa.close
+class a:
+   x = 2/67
 
-print('___________________________________')
-
-a= open("C:/Users/User/Desktop/python testes/testoterom.txt", "w", encoding="utf8")
-
-a.write('I got a gun, agaragan ')
-
-a.write('\n uno')
-a.close
+b=a()
+print('Outra classe ',b.x)
 
 print(' ')
-
-b = open("C:/Users/User/Desktop/python testes/testo.py", "w")
-b.write('i=1 \nwhile i<=10: \n  print(i,\'numeros\')   \n  i+=1')
-
-print(' ')
-
-
-
-
