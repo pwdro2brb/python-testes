3e21fd36c01710cbd4a01c5d06d8f915908b07bf
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-18 17:22:23 -0300

    Criando e escrevendo em outros arquivos no python.

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 print(' ')
 
-arqu_pa = open("C:/Users/User/Desktop/python testes/paises.txt", "r", encoding="utf8")#abre o outro arquivo nesse outro local
+arqu_pa = open("C:/Users/User/Desktop/python testes/paises.txt", "r", encoding="utf8")#abre o outro arquivo nesse outro local, fará a sua leitura, e considera a codificação do outro arquivo.
+
 print(arqu_pa.readline()[0])#inprime a primeira linha e a primeira letra do outro documento
 
 print(' ')
+
 print(arqu_pa.readline())#Agora ele irá inprimir a segunda linha do outro documento
-print(arqu_pa.readline(3))#terceira linha e os trés primeiros valores dela 
-print(' ')
-
-print(arqu_pa.readable())#inprime se o outro arquivo é "legivel"
-print(' ')
-
-for files in arqu_pa.readlines():#inprime todas as linhas restantes com espaço (quebra de linha)
-   print(files)
 arqu_pa.close
+
+print('___________________________________')
+
+a= open("C:/Users/User/Desktop/python testes/testoterom.txt", "w", encoding="utf8")
+
+a.write('I got a gun, agaragan ')
+
+a.write('\n uno')
+a.close
+
 print(' ')
+
+b = open("C:/Users/User/Desktop/python testes/testo.py", "w")
+b.write('i=1 \nwhile i<=10: \n  print(i,\'numeros\')   \n  i+=1')
+
+print(' ')
+
 
 
 
